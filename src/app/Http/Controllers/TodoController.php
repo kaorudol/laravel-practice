5b5b1ae906a497ffbae3895b2bd87f611abefa49
Honoka.A <h.tuhanyou@gmail.com>
2026-02-17 18:46:51 +0900

    feat:一括代入の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -17,21 +17,23 @@ class TodoController extends Controller
 
     public function create()
     {
-    return view('todo.create', ['content']);
-    dd('新規作成画面のルート実行！');
+    return view('todo', ['content']);
+    //dd('新規作成画面のルート実行！');
     }
 
     public function store(Request $request)
     {
-    $content = $request->input('content');
-    // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    $content = $request->all();
+    //dd($inputs);
+
     $todo = new Todo(); 
-    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-    $todo->content = $content;
-    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->fill($inputs);
     $todo->save();
+
     return redirect()->route('todo.index'); // 追記
     }
 }
+
+
 
 
